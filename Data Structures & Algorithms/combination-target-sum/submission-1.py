11c1c51e3019644d6567a66584572f61d00af08f
Lillian Wang <68929848+Skyler-Cloud@users.noreply.github.com>
2026-08-31 21:27:53 -0400

    Add: combination-target-sum - submission-1

---
+++ Data Structures & Algorithms/combination-target-sum/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        nums.sort()
+        memo={}
+
+        def comb(i,t):
+            if (i,t) in memo:
+                return memo[i,t]
+            if t==0:
+                return [[]]
+            if t < 0 or i>=len(nums) or nums[i]>target:
+                return []
+            out = []
+            for num_i in range(0,t//nums[i]+1):
+                out += [[nums[i]]*num_i+sublist for sublist in comb(i+1,t-nums[i]*num_i)]
+            memo[i,t]=out
+            return out
+        return comb(0,target)
+                
+
